45127289b8a5b39625d7e3d63272dc601328bd3a
hoganj3 <jsn.hogan@gmail.com>
2019-01-03 10:31:12 +0000

    Added remote MongoDB database, started reading message data from there, and displaying readable output on the front end

--- index.js
+++ index.js
@@ -2,7 +2,10 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
 const port = 3000
-const PouchDB = require('pouchdb')
+const mongoURL = 'mongodb://heroku_szpwsdc0:papf0959i5bi7dt0od3bvq751@ds253783.mlab.com:53783/heroku_szpwsdc0'
+const remoteDatabaseName = 'heroku_szpwsdc0'
+let messageConsumer = require('./messageConsumption')
+let sampleData = require('./sampleData')
 
 app.use("/", express.static(__dirname))
 app.use(bodyParser.urlencoded({ extended: false }))
@@ -10,11 +13,35 @@ app.use(bodyParser.json())
 app.set('views', __dirname + '/views')
 app.set('view engine', 'pug')
 
-let messageConsumer = require('./messageConsumption')
-let sampleData = require('./sampleData')
+const MongoClient = require('mongodb').MongoClient
+// MongoClient.connect(mongoURL,{ useNewUrlParser: true },function(err, db) {
+// 		if(!err) {
+// 			console.log("Consumer connecteded to DB")
+// 		}
+// 		let database = db.db(remoteDatabaseName)
+// 		// Try to create a collection in the DB, creation ignored if collection already exists
+// 		database.createCollection('messageStore', function(err, collection) {
+// 			if(!err) {
+// 				console.log("Created collection")
+// 			}
+// 			collection.insertOne(sampleData) // Insert message
+// 		})
+// 	})
+
+// mongodb://heroku_szpwsdc0:papf0959i5bi7dt0od3bvq751@ds253783.mlab.com:53783/heroku_szpwsdc0
+
 
 app.get('/', (req, res) => {
-	res.render('index', sampleData)
+	MongoClient.connect(mongoURL,{ useNewUrlParser: true },function(err, db) {
+		if(err) { return console.dir(err); }
+		let database = db.db(remoteDatabaseName)
+		let collection = database.collection('messageStore')
+
+		collection.find().toArray(function(err, items) {
+			console.log('Data retrieved from store:\n', items)
+			res.render('index', {messages: items})
+		})
+	})
 })
 
 app.post('/messageInput', function(request, response){
--- messageConsumption.js
+++ messageConsumption.js
@@ -1,11 +1,18 @@
-const PouchDB = require('pouchdb')
+const MongoClient = require('mongodb').MongoClient
 
 function consumeMessage(message){
-  let database = new PouchDB('http://localhost:3000/messageStore')
-  database.put(message)
-  database.info().then(function (info) {
-    console.log(info)
+  MongoClient.connect('mongodb://heroku_szpwsdc0:papf0959i5bi7dt0od3bvq751@ds253783.mlab.com:53783/heroku_szpwsdc0',
+      { useNewUrlParser: true }, function(err, database) {
+    if(!err) {
+      console.log("Consumer connecteded to DB")
+    }
+
+    // Try to create a collection in the DB, creation ignored if collection already exists
+    database.createCollection('messageStore', function(err, collection) {
+      collection.insert(message) // Insert message
+    })
   })
+
   return null
 }
 
